5a77204f5e99beec3bb0f9d8c38488df59384612
Shardul Bagade <bagadeshardul@gmail.com>
2014-05-06 14:07:19 -0400

    client side implemented

---
+++ public/js/lib/common.js
@@ -0,0 +1,69 @@
+var hostname, client;
+
+//hostname = window.location.hostname;
+hostname = '129.21.61.247';
+client   = new BinaryClient('ws://' + hostname + ':9000');
+
+
+function fizzle(e) {
+    e.preventDefault();
+    e.stopPropagation();
+}
+
+function emit(event, data, file) {
+    file       = file || {};
+    data       = data || {};
+    data.event = event;
+	
+	if(data.server_port !== undefined)
+	{
+		//
+		
+		
+		//client_get_video   = new BinaryClient('ws://' + hostname + ':' + data.server_port);
+		
+		client_get_video   = new BinaryClient('ws://' + data.server_port);
+		
+		client_get_video.on('open', function () {
+			
+			console.log("asdfasdfdsf");
+			//return client_get_video.send(file, data);
+			
+			client.send =  client_get_video.send(file, data);
+			
+			return client.send;
+			
+			
+		});
+		
+		
+		client_get_video.on('stream', function (stream) {
+	
+			//console.log("in stream");
+			
+			$video    = $('#video');
+			
+			video.download(stream, function (err, src) {
+				$video.attr('src', src);
+			});
+		});
+		
+		client_get_video.on('close', function() {
+			
+			client_get_video.close();
+		});
+		
+		
+		
+		
+	}
+	else
+	{
+		return client.send(file, data);
+	}
+	
+	
+
+    
+    
+}
---
+++ public/js/main.js
@@ -0,0 +1,157 @@
+$(document).ready(function () {
+    var $video, $box, $progress, $list;
+	var name;
+	
+	var host_ip = '129.21.61.247';
+	
+
+    $video    = $('#video');
+    $box      = $('#upload-box');
+    $progress = $('#progress');
+    $list     = $('#list');
+
+    $video.attr({
+        controls : true,
+        autoplay : true
+    });
+
+    client.on('open', function () {
+        video.list(setupList);
+
+        $box.on('dragenter', fizzle);
+        $box.on('dragover', fizzle);
+        $box.on('drop', setupDragDrop);
+    });
+
+    client.on('stream', function (stream) {
+	
+		console.log("in stream");
+		
+        video.download(stream, function (err, src) {
+            $video.attr('src', src);
+        });
+    });
+	
+	
+	
+	
+	
+	//var socket = io.connect('http://localhost:3000'); // connect to bootstrap
+	
+	//var socket = io.connect('http://129.21.61.0:3000'); // connect to bootstrap
+	var socket = io.connect('http://' + host_ip + ':3000'); // connect to bootstrap
+
+    function setupList(err, files) {
+        var $ul, $li;
+
+        $list.empty();
+        $ul   = $('<ul>').appendTo($list);
+
+        files.forEach(function (file) {
+            $li = $('<li>').appendTo($ul);
+            $a  = $('<a>').appendTo($li);
+
+            $a.attr('href', '#').text(file).click(function (e) {
+                fizzle(e);
+
+				name = $(this).text();
+				
+				console.log(name);
+				
+				//var coordinator = "";
+				
+				socket.emit('get_coordinator', { message: 'give me the video server coordinator'});
+				
+				
+				
+                
+            });
+        });
+    }
+
+    function setupDragDrop(e) {
+        fizzle(e);
+
+        var file, tx;
+
+        file = e.originalEvent.dataTransfer.files[0];
+        tx   = 0;
+
+        video.upload(file, function (err, data) {
+            var msg;
+
+            if (data.end) {
+                msg = "Upload complete: " + file.name;
+
+                video.list(setupList);
+            } else if (data.rx) {
+                msg = Math.round(tx += data.rx * 100) + '% complete';
+
+            } else {
+                // assume error
+                msg = data.err;
+            }
+
+            $progress.text(msg);
+            
+            if (data.end || data.err) {
+                setTimeout(function () {
+                    $progress.fadeOut(function () {
+                        $progress.text('Drop file here');
+                    }).fadeIn();
+                }, 5000);
+            }
+        });
+    }
+	
+	
+	
+	function get_video(port)
+	{
+		//console.log(port);
+		video.request(name, port);
+	}
+	
+	
+	
+	socket.on('reply_coordinator', function (data) {
+		
+		
+					console.log(data.server_port);
+		
+					//coordinator = data.server_port;
+					//socket.disconnect();
+					
+					// var socket_to_coordinator = io.connect('http://localhost:' + data.server_port);
+					// var socket_to_coordinator = io.connect('http://129.21.61.0:' + data.server_port);
+					var socket_to_coordinator = io.connect('http://' + host_ip + ':' + data.server_port);
+					
+					socket_to_coordinator.emit('get_videoserver', { message: 'give me the video server'});
+					
+					socket_to_coordinator.on('reply_videoserver', function (data_coord) {
+					
+						//console.log(data_coord.server_port);
+					
+						//video.request(name, data_coord.server_port);
+						
+						//socket_to_coordinator.disconnect();
+						
+						get_video(data_coord.server_port);
+					
+					});
+					
+		
+		
+		
+		
+	});
+	
+	
+	
+	
+	
+	
+	
+	
+	
+});
